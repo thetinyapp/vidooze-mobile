24ad7687489c9d76e7f722e9dd77c8fb79ec49f3
Adil Shaikh <adil@thetinyapp.com>
2024-07-24 22:24:47 +0530

    feat: updates build runner script

--- bin/generator.sh
+++ bin/generator.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-dart run build_runner build --delete-conflicting-outputs
+if [[ "$*" == *"--watch"* ]]; then
+    dart run build_runner watch --delete-conflicting-outputs
+   else
+    dart run  build_runner build --delete-conflicting-outputs
+fi
