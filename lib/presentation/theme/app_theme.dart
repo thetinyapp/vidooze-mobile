accf7cff70bb5fa5c27c67da9a7166c8c4275b40
Adil Shaikh <adil@thetinyapp.com>
2024-08-01 23:29:40 +0530

    feat: adds poppins font

--- lib/presentation/theme/app_theme.dart
+++ lib/presentation/theme/app_theme.dart
@@ -5,11 +5,16 @@ class AppTheme {
 
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
+    brightness: Brightness.light,
+    fontFamily: 'Poppins',
+    primaryColor: Colors.blue,
     scaffoldBackgroundColor: const Color.fromARGB(255, 240, 240, 240),
   );
 
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
+    brightness: Brightness.dark,
+    fontFamily: 'Poppins',
     scaffoldBackgroundColor: const Color.fromARGB(255, 1, 1, 1),
   );
 }
