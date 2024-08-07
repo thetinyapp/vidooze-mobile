adaae20a6514af0e4d4f360d7b9d137dc938e738
Adil Shaikh <adil@thetinyapp.com>
2024-08-08 02:38:38 +0530

    feat: removed unwanted code

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:vidooze_mobile/di/configure_di.dart';
-import 'package:vidooze_mobile/domain/repository/user_repository.dart';
 import 'package:vidooze_mobile/presentation/app_store.dart';
-import 'package:vidooze_mobile/presentation/extensions/app_router_extension.dart';
 import 'package:vidooze_mobile/presentation/router/app_router.dart';
 import 'package:vidooze_mobile/presentation/theme/app_theme.dart';
 
@@ -17,21 +15,6 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   final _appStore = locator.get<AppStore>();
   final _appRouter = AppRouter();
-
-  @override
-  void initState() {
-    super.initState();
-    final repository = locator.get<UserRepository>();
-    repository.isLoggedIn().then((result) {
-      result.onSuccess((it) {
-        if (it) {
-          context.goToAndReplace(const HomeRoute());
-        }
-      });
-    }).catchError((err) {
-      debugPrint(err);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
